1b5284a41a21c600b2cd48290a4bc47020a1afd3
uvsq22101020 <hadam92@outlook.fr>
2025-02-03 10:04:49 +0100

    hrihgjo

--- screens/HomePage.jsx
+++ screens/HomePage.jsx
@@ -28,10 +28,6 @@ export default function HomePage() {
         <script>
           const map = L.map('map').setView([48.864716, 2.349014], 13);
 
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          }).addTo(map);
-
           let routingControl;
 
           async function geocode(address) {
